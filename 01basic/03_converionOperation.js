1f3994bd7f2cd074cfcefe2ba77fd24464313de2
itsYashKhare <kharey02@gmail.com>
2024-01-02 19:14:52 +0530

    conversionoperation

---
+++ 01basic/03_converionOperation.js
@@ -0,0 +1,27 @@
+let score = "yash"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+//  "33abc" => NaN
+// true => 1 , false => 0
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// true => 1 , false => 0
+// "" => false
+// "yash" =>true
+
+
+let someNumber = 33
+ let stringNumber = String(someNumber)
+ console.log(typeof stringNumber);
+ console.log(stringNumber);
